69401c285e2b52017919f6b20160cc0160f472fc
Jobotron <Jobotron@users.noreply.github.com>
2025-04-13 20:36:06 -0700

    =functional POST

--- frontend/src/TaskManager.tsx
+++ frontend/src/TaskManager.tsx
@@ -1,6 +1,32 @@
 import KanbanComponent from "./KanbanComponent.tsx";
+import CreateKanbanTaskForm from "./CreateKanbanTaskForm.tsx";
 import Task from "./Task.ts";
 import {useEffect, useState} from "react";
+
+
+function createTask(title: string, status: string): Promise<Task> {
+    return fetch('http://localhost:8080/tasks', {
+        method: 'POST',
+        body: JSON.stringify({
+                "title": title,
+                "status": status,
+            }),
+        })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then((data: Task) => {
+            console.log('Task created:', data);
+            return data;
+        })
+        .catch((error) => {
+            console.error('Error creating task:', error)
+            throw error;
+        });
+}
 
 export default function TaskManager() {
     const [tasks, setTasks] = useState<Task[]>([]);
@@ -17,6 +43,9 @@ export default function TaskManager() {
         )
     }, []);
     return (
-        <KanbanComponent tasks={tasks} />
+        <div>
+            <KanbanComponent tasks={tasks} />
+            <CreateKanbanTaskForm onCreate={createTask} />
+        </div>
     )
 }
